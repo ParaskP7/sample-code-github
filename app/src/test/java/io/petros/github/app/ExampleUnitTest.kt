5045aeaeed39e2ac1e4847a0b966fb93cb912fb3
Petros Paraskevopoulos <petros.paraskevopoulos@yahoo.com>
2018-07-14 10:40:38 +0200

    [github] init: Add template main activity kotlin project

---
+++ app/src/test/java/io/petros/github/app/ExampleUnitTest.kt
@@ -0,0 +1,13 @@
+package io.petros.github.app
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExampleUnitTest {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+}
