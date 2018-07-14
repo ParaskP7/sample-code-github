fe3a0c89ad556b270343532018b51941b5580d48
Petros Paraskevopoulos <petros.paraskevopoulos@yahoo.com>
2018-07-14 12:15:13 +0200

    [github] init: Add leak canary
    Leak Canary is a memory leak detection library and its goal is to help
    detekt memory leaks early and mainly for debug builds.
    
    The current configuration disables leak canary for release builds.

---
+++ presentation/src/main/kotlin/io/petros/github/presentation/App.kt
@@ -0,0 +1,22 @@
+package io.petros.github.presentation
+
+import android.app.Application
+import com.squareup.leakcanary.LeakCanary
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (!initLeakCanary()) return
+    }
+
+    private fun initLeakCanary(): Boolean {
+        return if (LeakCanary.isInAnalyzerProcess(this)) {
+            false
+        } else {
+            LeakCanary.install(this)
+            true
+        }
+    }
+
+}
