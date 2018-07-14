0e9eb891955f406287034c0918d870ea7da795c8
Petros Paraskevopoulos <petros.paraskevopoulos@yahoo.com>
2018-07-14 12:20:28 +0200

    [github] init: Add strict mode configuration
    Strict mode helps developers detekt development issues early and forces
    them to fix those. Otherwise, the debug version of the app will
    complain at runtime on specific thread or vm policy violation.
    
    The most relevant example of how strict mode helps develop more robust
    apps, is when it complains about disk reads/writes violations that
    happen on the UI thread, forcing the developer to think twice about
    doing that so they put that work on a background thread to not block
    the UI thread.

--- presentation/src/main/kotlin/io/petros/github/presentation/App.kt
+++ presentation/src/main/kotlin/io/petros/github/presentation/App.kt
@@ -1,13 +1,17 @@
 package io.petros.github.presentation
 
 import android.app.Application
+import android.os.Build
+import android.os.StrictMode
 import com.squareup.leakcanary.LeakCanary
+import io.petros.github.BuildConfig
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
         if (!initLeakCanary()) return
+        initStrictMode()
     }
 
     private fun initLeakCanary(): Boolean {
@@ -16,6 +20,58 @@ class App : Application() {
         } else {
             LeakCanary.install(this)
             true
+        }
+    }
+
+    private fun initStrictMode() {
+        if (BuildConfig.DEBUG) {
+            setThreadPolicyToStrictMode()
+            setVmPolicyToStrictMode()
+        }
+    }
+
+    private fun setThreadPolicyToStrictMode() {
+        val strictModeBuilder = StrictMode.ThreadPolicy.Builder()
+            .detectDiskReads()
+            .detectDiskWrites()
+            .detectNetwork()
+            .detectCustomSlowCalls()
+            .penaltyLog()
+            .penaltyFlashScreen()
+            .penaltyDialog()
+        addAndroidMThreadPolicyToStrictMode(strictModeBuilder)
+        addAndroidOThreadPolicyToStrictMode(strictModeBuilder)
+        StrictMode.setThreadPolicy(strictModeBuilder.build())
+    }
+
+    private fun addAndroidMThreadPolicyToStrictMode(strictModeBuilder: StrictMode.ThreadPolicy.Builder) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            strictModeBuilder.detectResourceMismatches()
+        }
+    }
+
+    private fun addAndroidOThreadPolicyToStrictMode(strictModeBuilder: StrictMode.ThreadPolicy.Builder) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            strictModeBuilder.detectUnbufferedIo()
+        }
+    }
+
+    private fun setVmPolicyToStrictMode() {
+        val strictModeBuilder = StrictMode.VmPolicy.Builder()
+            .detectActivityLeaks()
+            .detectFileUriExposure()
+            .detectLeakedClosableObjects()
+            .detectLeakedSqlLiteObjects()
+            .detectLeakedRegistrationObjects()
+            .penaltyLog()
+        addAndroidOVmPolicyToStrictMode(strictModeBuilder)
+        StrictMode.setVmPolicy(strictModeBuilder.build())
+    }
+
+    private fun addAndroidOVmPolicyToStrictMode(strictModeBuilder: StrictMode.VmPolicy.Builder) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            strictModeBuilder.detectCleartextNetwork()
+                .detectContentUriWithoutPermission()
         }
     }
 
